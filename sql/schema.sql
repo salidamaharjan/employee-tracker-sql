2a24252755bb9750c6920491df038495f241ec21
salidamaharjan <salidamaharjan@outlook.com>
2024-01-27 11:50:52 -0600

    tables added to database

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS department(
     id INT NOT NULL PRIMARY KEY, 
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS role(
+    id INT NOT NULL PRIMARY KEY, 
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12, 4) NOT NULL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE IF NOT EXISTS employee(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
