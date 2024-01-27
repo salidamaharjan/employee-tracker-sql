ce73420367655c2cb0e579f3b2274cc9b1a12a2e
salidamaharjan <salidamaharjan@outlook.com>
2024-01-27 11:25:06 -0600

    creating table department

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,8 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE IF NOT EXISTS employee_tracker_db;
 
 USE employee_tracker_db;
+
+CREATE TABLE IF NOT EXISTS department(
+    id INT NOT NULL PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
