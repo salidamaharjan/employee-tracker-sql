0484482e9dd3ae674d5f5ea4b0d2689003cd1323
salidamaharjan <salidamaharjan@outlook.com>
2024-01-27 20:12:49 -0600

    removed unwanted folders

--- sql/query.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM role
-RIGHT JOIN department ON role.department_id = department.id;
-
-SELECT * FROM employee
-RIGHT JOIN role ON employee.role_id = role.id;
--- sql/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO department (name) 
-VALUES ("NURSING");
-
-INSERT INTO role (title, salary)
-VALUES ("RN", 80000);
-
-INSERT INTO employee (first_name, last_name)
-VALUES ("abcd", "xyz");
