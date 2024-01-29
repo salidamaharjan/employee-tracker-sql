8b65d0b8e24ba26274f9a37660102c0b92b51186
salidamaharjan <salidamaharjan@outlook.com>
2024-01-28 22:34:34 -0600

    read and save the department

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 // importing inquirer
 const inquirer = require("inquirer");
+const mysql = require("mysql2/promise");
+displayQuestion();
 
 /**
  * function created to display question and get the user choice
  */
 async function displayQuestion() {
-// using object destructuring to extract user choice
- const { choices } = await inquirer.prompt([
+  const db = await createConnection();
+  // using object destructuring to extract user choice
+  const { choices } = await inquirer.prompt([
     {
       name: "choices",
       type: "list",
       message: "What do you want to do?",
-      choices: ["Add a Department", "View all department"],
-    }
+      choices: ["Add a department", "View all department"],
+    },
   ]);
+  console.log(choices);
+  switch (choices) {
+    case "Add a department":
+      const { departmentName } = await inquirer.prompt([
+        {
+          name: "departmentName",
+          type: "input",
+          message: "Enter department name?",
+        },
+      ]);
+      // console.log(departmentName);
+      // using prepared statement to insert the name of department to db
+      const sql = "INSERT INTO department (name) VALUES (?)";
+      const values = [departmentName];
+      await db.execute(sql, values);
+      console.log("Department inserted");
+      break;
+    case "View all department":
+      const [result] = await db.query("SELECT * FROM department");
+      console.table(result);
+      break;
+  }
+  displayQuestion();
 }
 
-displayQuestion();
+async function createConnection() {
+  const db = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+    database: "employee_tracker_db",
+  });
+  return db;
+}
